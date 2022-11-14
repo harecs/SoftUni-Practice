fe535a94322fef4c217d412b0418715ce09ce49a
Zahari Lazarov <111045446+harecs@users.noreply.github.com>
2022-11-14 11:44:00 +0200

    Removed test input

--- JS Fundamentals/15. Arrays Advanced - Lab/08. Array Manipulations.js
+++ JS Fundamentals/15. Arrays Advanced - Lab/08. Array Manipulations.js
@@ -25,14 +25,3 @@ function manipulateArray(commands) {
 
     console.log(numbers.join(' '));
 }
-
-manipulateArray(['4 19 2 53 6 43',
-'Add 3',
-'Remove 2',
-'RemoveAt 1',
-'Insert 8 3']);
-manipulateArray(['6 12 2 65 6 42',
-'Add 8',
-'Remove 12',
-'RemoveAt 3',
-'Insert 6 2']);
