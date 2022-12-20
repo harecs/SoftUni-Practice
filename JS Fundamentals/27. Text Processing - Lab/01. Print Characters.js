2756344aeae1c433f8c6679d60ad405c21b4f7cf
Zahari Lazarov <111045446+harecs@users.noreply.github.com>
2022-12-20 13:11:38 +0200

    Added "27. Text Processing - Lab"

---
+++ JS Fundamentals/27. Text Processing - Lab/01. Print Characters.js
@@ -0,0 +1,5 @@
+function printCharacters(inputString) {
+    for (let i = 0; i < inputString.length; i++) {
+        console.log(inputString[i]);
+    }
+}
---
+++ JS Fundamentals/27. Text Processing - Lab/02. Substring.js
@@ -0,0 +1,4 @@
+function printSubstring(inputString, startIndex, count) {
+    let resultString = inputString.substring(startIndex, (startIndex + count));
+    console.log(resultString);
+}
