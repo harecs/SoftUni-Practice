057b33a1f46b0468f9e1908f43c73bc5946670f4
Zahari Lazarov <harilazaro@gmail.com>
2023-02-02 09:20:19 +0200

    Solve more exercises from DOM intro

--- JS Advanced - Exercises/08. DOM Introduction - Exercise/08. Generate Report/report.js
+++ JS Advanced - Exercises/08. DOM Introduction - Exercise/08. Generate Report/report.js
@@ -1,3 +1,23 @@
 function generateReport() {
-    //TODO
+    let inputElements = document.getElementsByTagName('input');
+    let headers = Array.from(inputElements);
+
+    let cellsElements = document.getElementsByTagName('tbody')[0].querySelectorAll('td');
+    let output = [];
+
+    console.log(headers);
+
+    for (let i = 0; i < cellsElements.length; i += headers.length) {
+        let resultObject = {};
+
+        headers.forEach((header, j) => {
+            if (header.checked) {
+                resultObject[header.name] = cellsElements[i + j].innerText;
+            }
+        });
+
+        output.push(resultObject);
+    }
+
+    document.getElementById('output').innerText = JSON.stringify(output);
 }
--- JS Advanced - Exercises/08. DOM Introduction - Exercise/09. Number Convertor/solution.js
+++ JS Advanced - Exercises/08. DOM Introduction - Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,30 @@
 function solve() {
+    addConvertToOptions();
 
-    //TODO...
+    document.getElementsByTagName('button')[0].addEventListener('click', convertNumber);
+
+    function convertNumber() {
+        let decimalNumber = Number(document.getElementById('input').value);
+        let formatToConvertTo = document.getElementById('selectMenuTo').value;
+        let outputElement = document.getElementById('result');
+
+        if (formatToConvertTo == 'binary') {
+            outputElement.value = decimalNumber.toString(2);
+        } else if (formatToConvertTo == 'hexadecimal') {
+            outputElement.value = decimalNumber.toString(16).toUpperCase();
+        }
+    }
+
+    function addConvertToOptions() {
+        let binaryOptionElement = document.createElement('option');
+        binaryOptionElement.value = 'binary';
+        binaryOptionElement.textContent = 'Binary';
+
+        let hexadecimalOptionElement = document.createElement('option');
+        hexadecimalOptionElement.value = 'hexadecimal';
+        hexadecimalOptionElement.textContent = 'Hexadecimal';
+
+        document.getElementById('selectMenuTo').appendChild(binaryOptionElement);
+        document.getElementById('selectMenuTo').appendChild(hexadecimalOptionElement);
+    }
 }
