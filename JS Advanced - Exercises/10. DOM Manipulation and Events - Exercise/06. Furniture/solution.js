0715bc84c0b378e25d18ce599fe66d0f43c06c8e
Zahari Lazarov <harilazaro@gmail.com>
2023-02-07 10:22:22 +0200

    Finish more exercises

--- JS Advanced - Exercises/10. DOM Manipulation and Events - Exercise/06. Furniture/solution.js
+++ JS Advanced - Exercises/10. DOM Manipulation and Events - Exercise/06. Furniture/solution.js
@@ -1,37 +1,74 @@
 function solve() {
-	let generateButtonElement = document.querySelector('#exercise button');
+	let buttonElements = document.getElementsByTagName('button');
+	let generateButtonElement = buttonElements[0];
+	let buyButtonElement = buttonElements[1];
 
 	generateButtonElement.addEventListener('click', onGenerateButtonClick);
+	buyButtonElement.addEventListener('click', onBuyButtonClick);
 
 	function onGenerateButtonClick(e) {
 		let input = JSON.parse(e.target.parentElement.querySelector('textarea').value);
-		let rowElement = document.createElement('tr');
-		let imageElement = document.createElement('img');
-		let nameElement = document.createElement('p');
-		let priceElement = document.createElement('p');
-		let decorationFactorElement = document.createElement('p');
-		let checkboxElement = document.createElement('input');
 
-		imageElement.setAttribute('src', input.img);
-		nameElement.textContent = input.name;
-		priceElement.textContent = input.price;
-		decorationFactorElement.textContent = input.decFactor;
-		checkboxElement.setAttribute('type', 'checkbox');
+		for (const line of input) {
+			let rowElement = document.createElement('tr');
+			let imageElement = document.createElement('img');
+			let nameElement = document.createElement('p');
+			let priceElement = document.createElement('p');
+			let decorationFactorElement = document.createElement('p');
+			let checkboxElement = document.createElement('input');
 
-		let rowContentElements = [
-			imageElement,
-			nameElement,
-			priceElement,
-			decorationFactorElement,
-			checkboxElement
-		];
-		
-		for (const rowContent of rowContentElements) {
-			let currentTd = document.createElement('td');
-			currentTd.appendChild(rowContent);
-			rowElement.appendChild(currentTd);
+			imageElement.setAttribute('src', line.img);
+			nameElement.textContent = line.name;
+			priceElement.textContent = line.price;
+			decorationFactorElement.textContent = line.decFactor;
+			checkboxElement.setAttribute('type', 'checkbox');
+
+			let rowContentElements = [
+				imageElement,
+				nameElement,
+				priceElement,
+				decorationFactorElement,
+				checkboxElement
+			];
+
+			for (const rowContent of rowContentElements) {
+				let currentTd = document.createElement('td');
+				currentTd.appendChild(rowContent);
+				rowElement.appendChild(currentTd);
+			}
+
+			document.querySelector('.table tbody').appendChild(rowElement);
+		}
+	}
+
+	function onBuyButtonClick(e) {
+		let outputTextAreaElement = e.target.previousElementSibling;
+		let checkedInputElements = document.querySelectorAll('input:checked');
+		let boughtFurnitureNames = [];
+		let totalPrice = 0;
+		let totalDecorationFactor = 0;
+		let averageDecorationFactor = 0;
+		let output = '';
+
+		for (const checkboxElement of checkedInputElements) {
+			let furnitureRowElement = checkboxElement.parentElement.parentElement;
+			let furnitureColumnElements = furnitureRowElement.children;
+
+			let furnitureName = furnitureColumnElements[1].textContent;
+			let furniturePrice = Number(furnitureColumnElements[2].textContent);
+			let furnitureDecorationFactor = Number(furnitureColumnElements[3].textContent);
+
+			boughtFurnitureNames.push(furnitureName);
+			totalPrice += furniturePrice;
+			totalDecorationFactor += furnitureDecorationFactor;
 		}
 
-		document.querySelector('.table tbody').appendChild(rowElement);
+		averageDecorationFactor = totalDecorationFactor / boughtFurnitureNames.length;
+
+		output += `Bought furniture: ${boughtFurnitureNames.join(', ')}\n`;
+		output += `Total price: ${totalPrice.toFixed(2)}\n`;
+		output += `Average decoration factor: ${averageDecorationFactor}`;
+
+		outputTextAreaElement.value = output;
 	}
 }
--- JS Advanced - Exercises/10. DOM Manipulation and Events - Exercise/07. Distance-Converter/distanceConverter.js
+++ JS Advanced - Exercises/10. DOM Manipulation and Events - Exercise/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,27 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+	document.getElementById('convert').addEventListener('click', onConvertButtonClick);
+
+	function onConvertButtonClick(e) {
+		let unitsInMeters = {
+			km: 1000,
+			m: 1,
+			cm: 0.01,
+			mm: 0.001,
+			mi: 1609.34,
+			yrd: 0.9144,
+			ft: 0.3048,
+			in: 0.0254
+		}
+
+		let inputDistance = Number(e.target.parentElement.querySelector('#inputDistance').value);
+		let unitToConvertFrom = e.target.parentElement.querySelector('#inputUnits').value;
+		let unitToConvertTo = e.target.parentElement.parentElement.querySelector('#outputUnits').value;
+		let outputFieldElement = e.target.parentElement.parentElement.querySelector('#outputDistance');
+
+		let inputDistanceInMeters = inputDistance * unitsInMeters[unitToConvertFrom];
+		let convetedDistance = inputDistanceInMeters / unitsInMeters[unitToConvertTo];
+
+		outputFieldElement.value = convetedDistance;
+	}
+
 }
