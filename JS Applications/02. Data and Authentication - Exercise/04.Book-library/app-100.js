09c03c125bd93d1e7ad8ca8e5e9ad1cf21198cfc
Zahari Lazarov <harilazaro@gmail.com>
2023-03-28 10:47:23 +0300

    Edit 04.Book-library

--- JS Applications/02. Data and Authentication - Exercise/04.Book-library/app-100.js
+++ JS Applications/02. Data and Authentication - Exercise/04.Book-library/app-100.js
@@ -1,5 +1,3 @@
-// 75/100 in judge but working better than app-100.js (In this version loading books after they are loaded doesn't work)
-
 attachEvents();
 
 function attachEvents() {
@@ -21,39 +19,24 @@ function attachEvents() {
         const title = document.querySelector('input[name="title"]').value;
         const author = document.querySelector('input[name="author"]').value;
 
-        try {
-            if (title != '' && author != '') {
+        if (title != '' && author != '') {
 
-                if (e.target.textContent === 'Submit') {
-                    await submitBook(title, author);
-                    clearInputFields();
-                } else if (e.target.textContent === 'Save') {
-                    await editBook(title, author);
-                    formButton.textContent = 'Submit';
-                    clearInputFields();
-                }
-            } else {
-                alert('All fields must be filled');
+            if (e.target.textContent === 'Submit') {
+                await submitBook(title, author);
+            } else if (e.target.textContent === 'Save') {
+                await editBook(title, author);
             }
-        } catch (error) {
-            console.error(error);
+        } else {
+            alert('All fields must be filled');
         }
 
-
     }
-
-    function clearInputFields() {
-        titleFieldElement.value = '';
-        authorFieldElement.value = '';
-    }
-
     async function onLoadBooks() {
         clearOldBooks();
 
         try {
             const res = await fetch(_url);
-            // const data = res.status === 200 ? await res.json() : new Error();
-            const data = res.ok ? await res.json() : new Error();
+            const data = res.status === 200 ? await res.json() : new Error();
             Object.values(data).forEach(bookInfo => addBookRow(bookInfo));
         } catch (error) {
             console.error(error);
@@ -90,7 +73,9 @@ function attachEvents() {
     }
 
     function clearOldBooks() {
-        Array.from(tbodyElement.children).forEach(child => child.remove());
+        if (tbodyElement.children) {
+            Array.from(tbodyElement.children).forEach(child => child.remove());
+        }
     }
 
     async function submitBook(title, author) {
@@ -102,9 +87,8 @@ function attachEvents() {
             };
 
             const res = await fetch(_url, options);
-            const data = res.ok ? await res.json() : new Error();
-            await onLoadBooks()
-            // addBookRow({ title, author, id: data._id });
+            res.ok ? await res.json() : new Error();
+            addBookRow({title, author});
         } catch (error) {
             console.error(error);
         }
@@ -116,15 +100,14 @@ function attachEvents() {
         const options = {
             'method': 'PUT',
             'Content-type': 'application/json',
-            'body': JSON.stringify({ title, author, id: idForEdit })
+            'body': JSON.stringify({ title, author })
         }
 
         const res = await fetch(`${_url}/${idForEdit}`, options);
-        const data = res.ok ? await res.json() : new Error();
-
-        await onLoadBooks();
-        // trElement.querySelector('td:nth-child(1)').textContent = title;
-        // trElement.querySelector('td:nth-child(2)').textContent = author;
+        res.ok ? await res.json() : new Error();
+        
+        trElement.querySelector('td:nth-child(1)').textContent = title;
+        trElement.querySelector('td:nth-child(2)').textContent = author;
     }
 
     async function onTbodyClick(e) {
@@ -138,7 +121,7 @@ function attachEvents() {
             }
         } else if (e.target.textContent === 'Edit') {
             formTitleElement.textContent = 'Edit FORM';
-
+            
             const currentRowElement = e.target.parentElement.parentElement;
             titleFieldElement.value = currentRowElement.querySelector('td:nth-child(1)').textContent;
             authorFieldElement.value = currentRowElement.querySelector('td:nth-child(2)').textContent;
