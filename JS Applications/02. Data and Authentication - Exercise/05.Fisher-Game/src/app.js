99d33eb0f475ea19273cbea00d28bec9944fb983
Zahari Lazarov <harilazaro@gmail.com>
2023-03-29 14:37:10 +0300

    Implement register functionality (src/register.js)

--- JS Applications/02. Data and Authentication - Exercise/05.Fisher-Game/src/app.js
+++ JS Applications/02. Data and Authentication - Exercise/05.Fisher-Game/src/app.js
@@ -1,1 +1,8 @@
 console.log('TODO:// Implement Home functionality');
+
+if (localStorage.key(0) == 'email') {
+    document.querySelector('nav p span').textContent = localStorage.getItem('email');
+    document.querySelector('nav div[id="guest"]').style.display = 'none';
+} else {
+    document.querySelector('nav div[id="user"]').style.display = 'none';
+}
--- JS Applications/02. Data and Authentication - Exercise/05.Fisher-Game/src/register.js
+++ JS Applications/02. Data and Authentication - Exercise/05.Fisher-Game/src/register.js
@@ -1,1 +1,38 @@
-console.log('TODO:// Implement Register functionality');
+const _url = 'http://localhost:3030/users/register';
+
+document.querySelector('nav div[id="user"]').style.display = 'none';
+
+document.querySelector('section form button').addEventListener('click', onRegister);
+
+async function onRegister(e) {
+    e.preventDefault();
+
+    const formData = new FormData(document.forms['register']);
+    const { email, password, rePass } = Object.fromEntries(formData);
+
+    if (email == '' || password == '' || rePass == '') {
+        alert('All form fields must be completed');
+    }
+
+    if (password !== rePass) {
+        alert(`Passwords don't match`);
+    }
+
+    const options = {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify({ email, password, rePass })
+    }
+
+    try {
+        const res = await fetch(_url, options);
+        const data = res.ok ? await res.json() : new Error();
+
+        localStorage.setItem('email', data.email);
+        localStorage.setItem('id', data._id);
+
+        location = './index.html'
+    } catch (err) {
+        console.error(err);
+    }
+}
