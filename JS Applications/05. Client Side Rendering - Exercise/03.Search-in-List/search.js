a144b03ea1a67ec2e161f4ac6c53bfca440da5d5
Zahari Lazarov <harilazaro@gmail.com>
2023-04-21 16:00:14 +0300

    Finish "03.Search-in-List" task

--- JS Applications/05. Client Side Rendering - Exercise/03.Search-in-List/search.js
+++ JS Applications/05. Client Side Rendering - Exercise/03.Search-in-List/search.js
@@ -1,3 +1,23 @@
-function search() {
-   // TODO
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+import { towns } from './towns.js';
+
+const template = (data) => html`<ul>${data.map(town => html`<li>${town}</li>`)}</ul>`;
+render(template(towns), document.querySelector('#towns'));
+
+document.querySelector('article button').addEventListener('click', onSearch);
+
+function onSearch(e) {
+    const searchText = e.target.previousElementSibling.value;
+
+    document.querySelectorAll('li').forEach(ele => {
+        if (ele.textContent.includes(searchText)) {
+            ele.classList.add('active');
+        } else {
+            ele.classList.remove('active');
+        }
+    });
+
+    const matchesCount = document.querySelectorAll('.active').length;
+    
+    document.querySelector('#result').textContent = `${matchesCount} matches found`;
 }
