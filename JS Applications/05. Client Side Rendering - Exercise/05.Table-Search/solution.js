4cf1943b0c707f108c42f84ee9af662c0c224a7e
Zahari Lazarov <harilazaro@gmail.com>
2023-04-25 15:51:08 +0300

    Finish "05.Table-Search" task

--- JS Applications/05. Client Side Rendering - Exercise/05.Table-Search/solution.js
+++ JS Applications/05. Client Side Rendering - Exercise/05.Table-Search/solution.js
@@ -1,8 +1,59 @@
-function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+import { html, render } from '../node_modules/lit-html/lit-html.js';
 
-   function onClick() {
-      //   TODO:
+render(template(await getTableData()), document.querySelector('.container tbody'));
 
+const searchFieldElement = document.querySelector('#searchField');
+document.querySelector('#searchBtn').addEventListener('click', onSearch);
+
+async function getTableData() {
+   try {
+      const url = 'http://localhost:3030/jsonstore/advanced/table';
+      const options = {
+         method: 'GET',
+         headers: { 'Content-Type': 'application/json' }
+      };
+
+      const response = await fetch(url, options)
+      
+      if (!response.ok || response.status == 204) {
+         const error = await response.json();
+         throw error;
+      }
+
+      return response.json();
+   } catch (err) {
+      console.error(err);
    }
+}
+
+function template(data) {
+   return Object.values(data)
+      .map(x => html`<tr>
+                        <td>${x.firstName + ' ' + x.lastName}</td>
+                        <td>${x.email}</td>
+                        <td>${x.course}</td>
+                     </tr>`);
+}
+
+function onSearch() {
+   unhighlightTable();
+   const searchText = searchFieldElement.value;
+
+   if (!searchText) {
+      return;
+   }
+
+   searchFieldElement.value = '';
+
+   Array.from(document.querySelectorAll('td'))
+      .forEach(element => {
+         if (element.textContent.toLowerCase().includes(searchText.toLowerCase())) {
+            element.parentElement.classList.add('select');
+         }
+   });
+}
+
+function unhighlightTable() {
+   Array.from(document.querySelectorAll('.select'))
+      .forEach(element => element.classList.remove('select'));
 }
